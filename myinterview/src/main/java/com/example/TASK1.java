f0ba740a503785c769cdb596670286a9a198307c
Raaffss <86023039+Raaffss@users.noreply.github.com>
2024-04-17 14:19:15 -0300

    Update TASK1.java

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -13,6 +13,38 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
- 
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Digite uma palavra ou frase para verificar se é um palíndromo:");
+            String entrada = scanner.nextLine();
+            if (isPalindromo(entrada)) {
+                System.out.println("A entrada é um palíndromo.");
+            } else {
+                System.out.println("A entrada não é um palíndromo.");
+            }
+            System.out.println("Deseja verificar outro palíndromo? (S/N)");
+            String opcao = scanner.nextLine();
+            if (!opcao.equalsIgnoreCase("S")) {
+                continuar = false;
+            }
+        }
+        scanner.close();
+    }
+
+    public static boolean isPalindromo(String str) {
+        String strLimpa = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int esquerda = 0;
+        int direita = strLimpa.length() - 1;
+        while (esquerda < direita) {
+            if (strLimpa.charAt(esquerda) != strLimpa.charAt(direita)) {
+                return false;
+            }
+            esquerda++;
+            direita--;
+        }
+        return true;
+    }
 }
